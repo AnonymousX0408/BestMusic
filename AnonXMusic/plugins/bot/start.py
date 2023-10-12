3a9e330fb53c8d49f88ac58fd1c0a97ac8087dea
Venomxboss <142314403+Venomxboss@users.noreply.github.com>
2023-10-12 13:32:51 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -32,7 +32,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgQAAx0CXw8FEQACCeZk8sF7-lXmyfZPbtI6TmBclC8BEwACmAwAAvC0iFIR_WWaFCiz1zAE")
+            await message.reply_sticker("CAACAgUAAx0CWrNeBQABF5F6ZSekjDIxQEvXv_l1E_dk9kbpxX4AAqkKAAJ5rxhVsXw8lCOqlEUeBA")
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
@@ -85,7 +85,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgQAAx0CXw8FEQACCeZk8sF7-lXmyfZPbtI6TmBclC8BEwACmAwAAvC0iFIR_WWaFCiz1zAE")
+        await message.reply_sticker("CAACAgUAAx0CWrNeBQABF5F6ZSekjDIxQEvXv_l1E_dk9kbpxX4AAqkKAAJ5rxhVsXw8lCOqlEUeBA")
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
